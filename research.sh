0f60f68d45c32a0e2d47a397644456897ab60349
eukaryo <c3375c15@gmail.com>
2019-02-28 00:58:23 +0900

    Update research.sh
    lol

--- research.sh
+++ research.sh
@@ -4,4 +4,4 @@ python3 random_dataset_generator.py isolen > dataset-iso-len.txt
 python3 iso_len_time_experiment.py dataset-iso-len.txt > result-isolen-time.txt
 python3 random_dataset_generator.py isodist > dataset-iso-dist.txt
 python3 iso_dist_time_experiment.py dataset-iso-dist.txt > result-isodist-time.txt
-
+python3 s151Rfam-localminima-generator.py > s151-localminima-dataset.txt
