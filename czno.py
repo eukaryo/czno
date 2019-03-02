fff0f0c6cc13862e98220d523932d0ec0ac74e5d
eukaryo <c3375c15@gmail.com>
2019-03-03 05:59:14 +0900

    Update czno.py

--- czno.py
+++ czno.py
@@ -101,7 +101,7 @@ def getMinBarrierPathDijkstra(sequence, structure1, structure2):
     #diff_pos_list[x]を変化させるとき、予めdiff_pos_list[y]を変化させていなければならない。
     #
     #(1)(i,j)が塩基対を組んでいるとき、(i,k)や(k,j)が同時に組むことは出来ない
-	#(2)pseudoknot禁止
+    #(2)pseudoknot禁止
     #これらを満たすためにprerequisite_flagが要請される。
     prerequisite_flag = np.zeros(H, dtype='uint64')
     for i in range(H):
@@ -110,11 +110,11 @@ def getMinBarrierPathDijkstra(sequence, structure1, structure2):
             if diff_pos_list[j] not in bp_set2: continue
 
             #iを踏んでからjを踏む必要がある場合、obstacle[j] |= 1 << i とする。
-			#そのようなケースは、(max-loop制約を無視すると)
-			#(1)スタート構造でiが組まれていてjは組まれていない
-			#(2)ゴール構造ではiは組まれておらずjが組まれている
-			#ようなケースに限られる。
-			#(max-loop制約においては、先に外すことがinvalidになるケースがあるが、今回はRNAevalを使うので問題ない。)
+            #そのようなケースは、(max-loop制約を無視すると)
+            #(1)スタート構造でiが組まれていてjは組まれていない
+            #(2)ゴール構造ではiは組まれておらずjが組まれている
+            #ようなケースに限られる。
+            #(max-loop制約においては、先に外すことがinvalidになるケースがあるが、今回はRNAevalを使うので問題ない。)
 
             if isExclusive(diff_pos_list[i], diff_pos_list[j]):
                 prerequisite_flag[j] = np.bitwise_or(prerequisite_flag[j], np.uint64(2**i))
@@ -124,6 +124,9 @@ def getMinBarrierPathDijkstra(sequence, structure1, structure2):
 
     #energy[x]は構造xのエネルギーの値
     energy = np.full(2**H, np.inf)
+	
+    #exactは[x]のResultが確定したかどうか
+    exact = np.full(2**H, False)
 
     #Dijkstraは二分ヒープで、要素を小さい順に出す。
     #Dijkstraの要素は(部分解、-時刻、構造のインデックス)
@@ -134,20 +137,19 @@ def getMinBarrierPathDijkstra(sequence, structure1, structure2):
     energy[0] = energyOfStr(sequence, structure1)
     heapq.heappush(Dijkstra, (energy[0], count, np.uint64(0)))
     while len(Dijkstra) > 0:
-        x = heapq.heappop(Dijkstra)
-        index = np.uint64(x[2])
-        if result[index] != np.inf: continue
-        result[index] = x[0]
+        (solution, _, index) = heapq.heappop(Dijkstra)
+        if exact[index] == True: continue
+        exact[index] = True
+        result[index] = solution
         if index == np.uint64(2 ** H - 1): break
-        for i in range(H):
-            if np.bitwise_and(index, np.uint64(2**i)) != np.uint64(0): continue
-            if np.bitwise_and(index, prerequisite_flag[i]) != prerequisite_flag[i]: continue
-            new_index = np.bitwise_or(index, np.uint64(2**i))
-            if result[new_index] != np.inf: continue
+        for b in [np.uint64(2**i) for i in range(H) if np.bitwise_and(index, np.uint64(2**i)) == np.uint64(0) and np.bitwise_and(index, prerequisite_flag[i]) == prerequisite_flag[i]]:
+            new_index = np.bitwise_or(index, b)
+	    if exact[new_index] == True: continue
             if energy[new_index] == np.inf:
-                new_structure = makeDotBracket(structure1, bp_set1, [diff_pos_list[x] for x in range(H) if np.bitwise_and(new_index, np.uint64(2**x)) != np.uint64(0)])
-                energy[new_index] = energyOfStr(sequence, new_structure)
+                energy[new_index] = energyOfStr(sequence, makeDotBracket(structure1, bp_set1, [diff_pos_list[x] for x in range(H) if np.bitwise_and(new_index, np.uint64(2**x)) != np.uint64(0)]))
             new_result = max(result[index], energy[new_index])
+	    if new_result >= result[new_index]: continue
+	    result[new_index] = new_result
             count -= 1
             heapq.heappush(Dijkstra, (new_result, count, new_index))
 
@@ -161,6 +163,7 @@ def getMinBarrierPathDijkstra(sequence, structure1, structure2):
             if np.bitwise_and(index, np.uint64(2**i)) == np.uint64(0): continue
             back_index = np.bitwise_xor(index, np.uint64(2**i))
             if np.bitwise_and(back_index, prerequisite_flag[i]) != prerequisite_flag[i]: continue
+            if exact[back_index] == False: continue
             if result[index] != max(energy[index], result[back_index]): continue
             answer.append(makeDotBracket(structure1, bp_set1, [diff_pos_list[x] for x in range(H) if np.bitwise_and(back_index, np.uint64(2**x)) != np.uint64(0)]))
             index = back_index
